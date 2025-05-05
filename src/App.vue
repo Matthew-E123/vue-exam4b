90028a07372379e397b9836da9b3cd21bca6d72f
Matthew-E123 <me0979389@otc.edu>
2025-05-05 12:19:28 -0500

    Final part 2 done

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script>
+import Nav from './components/Nav.vue'
+export default {
+  components: {
+    Nav
+  },
+  data() {
+    return {
+      plants: [
+        {id: 1, name: "Sunflower", type: "Outdoor", instock: true},
+        {id: 2, name: "Lavender", type: "Indoor", instock: true},
+        {id: 3, name: "Sweet Potato Vines", type: "Outdoor", instock: false}
+      ],
+      filterType: "All"
+    }
+  },
+  methods: {
+    handleFilter(type){
+      this.filterType = type;
+      console.log(this.filterType)
+    }
+  }
+}
+</script>
+
+
+<template>
+  <Nav @filter-plants="handleFilter"></Nav>
+  <h1>Plant Store App  </h1>
+  <router-view :plants="plants" :filterType="filterType" />
+</template>
+
---
+++ src/components/Nav.vue
@@ -0,0 +1,18 @@
+<template>
+    <nav>
+        <router-link to="/">Home</router-link>
+        <router-link to="/plants">View Plants</router-link>
+
+        <select @change="$emit('filter-plants', $event.target.value)">
+            <option value="All">All</option>
+            <option value="Indoor">Indoor</option>
+            <option value="Outdoor">Outdoor</option>
+        </select>
+    </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
